23ba101c09afb595abace4ae23c214ed595f8012
kenyi herry alcantara astete <kalcantaraa@uni.pe>
2022-07-18 09:59:27 -0500

    finish2

--- src/main/java/Aplication.java
+++ src/main/java/Aplication.java
@@ -3,12 +3,27 @@ public class Aplication {
     public static void main(String[] args) {
         Director director = new Director();
 
-        //Building a car
+        //Building a suv car  without a director
+        CarBuilder builder0 = new CarBuilder();
+        builder0.reset();
+        builder0.setEngine(new SUVEngine("suv"));
+        builder0.setSets(9);
+        builder0.setGPS(true);
+        builder0.setTripComputer(false);
+
+
+        //Building a manual usv car using a director
+        CarManualBuilder builder0Manual = new CarManualBuilder();
+        director.constructorSUV(builder0Manual);
+        Manual manual0 = builder0Manual.getResult();
+        imprimeManual(manual0);
+
+        //Building a sport car using a director
         CarBuilder builder = new CarBuilder();
         director.constructSportCar(builder);
         Car car = builder.getResult();
 
-        //Building a manual
+        //Building a manual sport car using a director
         CarManualBuilder builderManual = new CarManualBuilder();
         director.constructSportCar(builderManual);
         Manual manual = builderManual.getResult();
@@ -17,7 +32,7 @@ public class Aplication {
     }
 
     static void imprimeManual(Manual manual){
-        System.out.println("Manual car:");
+        System.out.println("\nManual car:");
         System.out.println("Name:"+manual.engine);
         System.out.println("NumberSeats:"+manual.numberSeats);
         System.out.println("Gps:"+manual.gps);
